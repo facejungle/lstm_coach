1231ae7f677b879141e758c8825ca27b31d6e531
Andrey Fomichev <sqinx@ya.ru>
2023-10-28 03:15:34 +0300

    Add function
    Add function intruments to csv

--- shared/utils/async_utils.py
+++ shared/utils/async_utils.py
@@ -27,3 +27,11 @@ async def write_csv(data, file_path):
         async with aiofiles.open(file_path, mode='w') as file:
             csv_content = data.to_csv(index=False)
             await file.write(csv_content)
+
+
+async def instrumentsToCsv(markets: list, instruments: list, filepath: str):
+    file = await read_csv(filepath)
+    df = pd.DataFrame({"market": markets, "instrument": instruments})
+    if not file.equals(df):
+        await write_csv(df, filepath)
+        print('Update: ', filepath)
